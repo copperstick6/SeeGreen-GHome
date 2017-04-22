c981011721cbd456b7924aa37419743334f576ce
copperstick6 <copperstick6@gmail.com>
2017-04-22 07:06:41 -0500

    added all files

--- app.py
+++ app.py
@@ -33,15 +33,29 @@ def next_round():
 def initRound():
     next_msg = render_template('Guess1')
     session.attributes['answers'] = int(session.attributes['answers']) + 1
-    return statement(next_msg)
+    return question(next_msg)
 
 @ask.intent("AnswerIntent", convert={'first': int})
 
 def answer(first):
-    if(int(session.attributes['answers']) < 2):
+    if int(session.attributes['answers']) < 2:
         return statement("Invalid instruction")
+    elif int(session.attributes['answers']) == 2:
+        input = int(first)
+        if input == 1:
+            render question(render_template('compostResponse'))
+        elif input == 2:
+            render_question(render_template('recyclingResponse'))
+        elif input == 3:
+            render_question(render_template('recyclingResponse'))
+        elif input == 4:
+
+        elif input == 5:
+
     msg = render_template('win')
-    return statement(msg)
+    return question(msg)
+
+@ask.intent("CompostIntent")
 
 
 if __name__ == '__main__':
